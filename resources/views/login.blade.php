36357e0076c39d7e21946b1e36e84d95d5a3e170
shahidx0x <dev.imshahid@gmail.com>
2024-03-30 17:46:27 +0600

    feat : tailwind framwork added and admin login route created

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h2>this is login form</h2>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\admin\adminLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/login')
+Route::get('/admin/login',[adminLoginController::class,'index'])->name('admin.login');
